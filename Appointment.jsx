54c69371cdf6e1a1c271206c8b1f801059e95780
hema-9059 <hravi6036@gmail.com>
2026-09-18 11:52:08 +0530

    Add files via upload

---
+++ Appointment.jsx
@@ -0,0 +1,12 @@
+const Appointment = () => {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-3xl font-bold text-indigo-600 mb-4">
+        Book Appointment
+      </h1>
+      <p className="text-gray-600">Here you will show doctors list and booking form.</p>
+    </div>
+  );
+};
+
+export default Appointment;
